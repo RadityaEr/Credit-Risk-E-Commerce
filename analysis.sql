25c7b8949b866c126081e971869d71862e556440
Raditya Erlang Arkananta <radityaarkananta@gmail.com>
2025-06-19 11:54:39 +0700

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,4 +1,3 @@
--- PostgreSQL: Pastikan order_date jadi DATE
 WITH rfm AS (
   SELECT
     customer_id,
@@ -7,5 +6,27 @@ WITH rfm AS (
     SUM(payment_value) AS monetary
   FROM e_commerce_transactions
   GROUP BY customer_id
+),
+ranked AS (
+  SELECT
+    customer_id,
+    recency,
+    frequency,
+    monetary,
+    NTILE(5) OVER (ORDER BY recency ASC) AS r_score,
+    NTILE(5) OVER (ORDER BY frequency DESC) AS f_score,
+    NTILE(5) OVER (ORDER BY monetary DESC) AS m_score
+  FROM rfm
 )
-SELECT * FROM rfm;
+SELECT
+  *,
+  CASE
+    WHEN r_score = 5 AND f_score = 5 AND m_score = 5 THEN 'Champions'
+    WHEN r_score >= 4 AND f_score >= 4 THEN 'Loyal Customers'
+    WHEN r_score >= 3 AND f_score >= 3 THEN 'Potential Loyalist'
+    WHEN r_score >= 3 AND f_score <= 2 THEN 'Needs Attention'
+    WHEN r_score <= 2 AND f_score >= 4 THEN 'At Risk'
+    WHEN r_score = 1 AND f_score = 1 THEN 'Lost'
+    ELSE 'Others'
+  END AS segment
+FROM ranked;
